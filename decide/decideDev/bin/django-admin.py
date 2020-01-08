626d786e25cfc967bb73ebeeed5263683b39b4e1
ManuelC <manuelc.p.g@gmail.com>
2020-01-08 18:44:41 +0100

    Cambios necesarios apra ejecutar en un venv de python

---
+++ decide/decideDev/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/equipo/Escritorio/EGC-20T-manpergut/egc20testT/decide/decideDev/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
